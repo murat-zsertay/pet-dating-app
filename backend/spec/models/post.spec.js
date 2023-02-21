b046bb8a2cef074e937bdd43bb7d9bd5b5e84a5b
Murat <murat.sertay@icloud.com>
2023-02-21 14:56:17 +0000

    Project setup - adding in posts to the backend

---
+++ backend/spec/models/post.spec.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+import "../mongodb_helper";
+import {Post} from "../../models/post";
+
+describe("Post model", () => {
+  beforeEach((done) => {
+    mongoose.connection.collections.posts.drop(() => {
+      done();
+    });
+  });
+
+  it("has a message", () => {
+    let post = new Post({ message: "some message" });
+    expect(post.message).toEqual("some message");
+  });
+
+  it("can list all posts", (done) => {
+    Post.find((err, posts) => {
+      expect(err).toBeNull();
+      expect(posts).toEqual([]);
+      done();
+    });
+  });
+
+  it("can save a post", (done) => {
+    let post = new Post({ message: "some message" });
+
+    post.save((err) => {
+      expect(err).toBeNull();
+
+      Post.find((err, posts) => {
+        expect(err).toBeNull();
+
+        expect(posts[0]).toMatchObject({ message: "some message" });
+        done();
+      });
+    });
+  });
+
+  it("returns the date and time of when a Post was created", async () => {
+    let post = new Post({ message: "some message"});
+    expect(post.createdAt).not.toBeNull()
+  });
+
+});
