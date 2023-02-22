d8d9927c06931e17fe73c64bd28100db5c9ef494
Murat <murat.sertay@icloud.com>
2023-02-22 16:12:42 +0000

    changes to config so workflow runs

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -2,18 +2,15 @@ module.exports = {
   apps : [{
     script: 'app.js',
     watch: '.',
-    "env_test": {
-      NODE_ENV: "test",
-      JWT_SECRET:"SUPER_SECRET",
-      PORT:8080,
-      MONGODB_URL:"mongodb://0.0.0.0:27017/pet_test"
-    },
-    "env_production": {
-      NODE_ENV: "production",
-    },
-    "env_development": {
-      NODE_ENV: "development",
-    }
+    // "env_test": {
+    //   NODE_ENV: "test",
+    // },
+    // "env_production": {
+    //   NODE_ENV: "production",
+    // },
+    // "env_development": {
+    //   NODE_ENV: "development",
+    // }
   }],
 
 };
