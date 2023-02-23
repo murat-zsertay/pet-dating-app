095301508bf2769f2ba6862a2c0ab96fb071be3e
Murat <murat.sertay@icloud.com>
2023-02-23 07:21:14 +0000

    Updated backend pm2 configuration

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,15 +1,22 @@
 module.exports = {
   apps : [{
+    name:"Express backend",
     script: 'app.js',
     watch: '.',
-    "env.test.local": {
+    "env_test": {
       NODE_ENV: "test",
+      JWT_SECRET:"SUPER_SECRET",
+      PORT:8080,
+      MONGODB_URL:"mongodb://0.0.0.0:27017/pet_test"
     },
-    "env.production.local": {
+    "env_production": {
       NODE_ENV: "production",
     },
-    "env.development.local": {
+    "env_development": {
       NODE_ENV: "development",
+      JWT_SECRET:"SUPER_SECRET",
+      MONGODB_URL:"mongodb://0.0.0.0:27017/pet_development",
+      PORT:8080
     }
   }],
 
