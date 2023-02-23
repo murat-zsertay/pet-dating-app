3359e7ccbbdd7aa41b441d17acfabcf97c8c5b07
Murat <murat.sertay@icloud.com>
2023-02-23 08:26:48 +0000

    Added in startServer.sh script to aid in starting both servers

--- bin/startServer.sh
+++ bin/startServer.sh
@@ -1,7 +1,24 @@
 #! /bin/bash
-cd ..
-# shellcheck disable=SC2164
-cd "$1"
-pm2 start ecosystem.config.cjs --env "$2"
-pm2 save
-# Export the function so it can be used in other scripts
+# Starts a pm2 process in a particular environment depending on the environment
+# variables passed in
+# $1 would be the first variable and the directory where the ecosystem.js
+# Starts a server in a particular environment depending on the environment
+# variable passed in
+
+
+# Determine the directory the script is being run from
+current_dir=$(pwd)
+
+
+if [[ $current_dir == *"/frontend"* || $current_dir == *"/backend"* ]]; then
+  cd ..
+  pm2 start ecosystem.config.cjs --env "$1"
+  pm2 save
+
+  cd $(basename "$current_dir")
+else
+  echo "Unknown directory"
+  exit 1
+fi
+
+# Start the service using the appropriate ecosystem file
