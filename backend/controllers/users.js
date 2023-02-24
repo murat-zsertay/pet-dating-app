1651db44e5ec6b38f42c7abe0c74a9c9eab9fe0a
Murat Sertay <muzzachan@me.com>
2023-02-24 17:25:59 +0000

    Add pets router functionality

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,34 +1,33 @@
-import {User} from "../models/user.js";
+import { User } from '../models/user.js'
 
 export const UsersController = {
   Create: async (req, res) => {
-    const existingUser = await User.exists({ email: req.body.email });
+    const existingUser = await User.exists({ email: req.body.email })
     if (existingUser) {
-      return res.status(400).json({ message: "Email already in use" });
+      return res.status(400).json({ message: 'Email already in use' })
     }
-    const newUser = new User(req.body);
+    const newUser = new User(req.body)
     newUser.save((err) => {
       if (err) res.status(400).json({ message: err.message })
-      else res.status(201).json({
-          message: "Thanks! your account has been successfully created"
-        });
-
-    });
+      else {
+        res.status(201).json({
+          message: 'Thanks! your account has been successfully created'
+        })
+      }
+    })
   },
 
   FindUserById: (req, res) => {
-        // Extract user_id from params (Taken from :post_id in the url)
-        let user_id = req.params.user_id
-        // Use user_id to find particular post
-        User.findById({_id:user_id}, async (err, user) => {
-            if (err) {
-                throw err;
-            }
-            // const token = await TokenGenerator.jsonwebtoken(req.user_id)
-            // returns a body containing the post object and token string
-            res.status(200).json({user});
-        });
-    },
-};
-
-
+    // Extract user_id from params (Taken from :post_id in the url)
+    const user_id = req.params.user_id
+    // Use user_id to find particular post
+    User.findById({ _id: user_id }, async (err, user) => {
+      if (err) {
+        throw err
+      }
+      // const token = await TokenGenerator.jsonwebtoken(req.user_id)
+      // returns a body containing the post object and token string
+      res.status(200).json({ user })
+    })
+  }
+}
