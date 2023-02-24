77f3895724c3af9bd5767549602a8cad1bcb9a74
Murat Sertay <muzzachan@me.com>
2023-02-24 17:26:48 +0000

    Update FindPetsPage element name

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -3,22 +3,18 @@ import LoginForm from '../auth/LoginForm'
 import SignUpForm from '../user/SignUpForm'
 import FindPetsPage from '../findPetsPage/FindPetsPage';
 import NavBar from '../navBar/navBar';
-import {
-    useNavigate,
-    Routes,
-    Route,
-} from "react-router-dom";
+import {Route, Routes, useNavigate,} from "react-router-dom";
 
 const App = () => {
     return (
         <>
             <NavBar/>
             <Routes>
-                <Route path='/login' element={<LoginForm navigate={useNavigate()} />} />
-                <Route path='/' element={<SignUpForm navigate={useNavigate()} />} />
-                <Route path='/signup' element={<SignUpForm navigate={useNavigate()} />} />
-                <Route path='/findPetsPage' element={<FindPetsPage navigate={useNavigate()} />} />
-            </Routes> 
+                <Route path='/login' element={<LoginForm navigate={useNavigate()}/>}/>
+                <Route path='/' element={<SignUpForm navigate={useNavigate()}/>}/>
+                <Route path='/signup' element={<SignUpForm navigate={useNavigate()}/>}/>
+                <Route path='/findPetsPage' element={<FindPetsPage navigate={useNavigate()}/>}/>
+            </Routes>
         </>
     );
 }
