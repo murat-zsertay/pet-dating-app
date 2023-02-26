36d2563cd5eefbc37b8e3058444ee87c033e6e2b
charlie lambert <char.lam@ntlworld.com>
2023-02-26 23:26:39 +0000

    User profile edit form with html done, methods to do

--- frontend/src/components/profileEditor/ProfileEditor.js
+++ frontend/src/components/profileEditor/ProfileEditor.js
@@ -1,29 +1,59 @@
 import React, { useState, useEffect } from "react";
+import PetForm
 
 
-const ProfileEditor = () => {
-  const [user, setUser] = useState(null);
+const ProfileEditor = (user) => {
   const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [updatedUser, setUpdatedUser] = useState(user)
 
-  const user_id = window.localStorage.getItem("user_id");
-  const fetchUser = async (user_id) => {
+  const updateUser = async (user_id) => {
     try {
-      const response = await fetch(`/users/${user_id}`);
+      const response = await fetch(`/users/${user_id}`,{
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ updateUser }),
+      });
       const data = await response.json();
-      setUser(data.user);
+      console.log(data)
+      setToken(data.token)
     } catch (error) {
       console.log(error);
     }
   };
-  useEffect(() => { 
-    fetchUser(user_id); 
-  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(user);
+    updateUser(user.user_id)
+  };
 
   if (!user) {
     return <div></div>;
   } else {
     return (
-      <div className="profile">
+      <div className="profile-editor">
+        <form className="editor-form" onSubmit={handleSubmit}>
+          <input className="form_field" id="email" type="text" value={user.email} onChange={handleEmailChange} />
+          <label className="form_label" htmlFor="email">Email</label>
+          <input className="form_field" id="first-name" type="text" value={user.firstName} onChange={handleFirstNameChange} />
+          <label className="form_label" htmlFor="first-name">First Name</label>
+          <input className="form_field" id="last-name" type="text" value={user.lastName} onChange={handleLastNameChange} />
+          <label className="form_label" htmlFor="last-name">First Name</label>
+          <input className="form_field" id="postcode" type="text" value={user.postcode} onChange={handlePostcodeChange} />
+          <label className="form_label" htmlFor="postcode">Postcode</label>
+          <input className="form_field" id="postcode" type="password" value={user.password} onChange={handlePasswordChange} />
+          <label className="form_label" htmlFor="password">Password</label>
+          <div className="user-pets">
+            {user.pets.map((pet, index) => (
+              <PetForm key={index} pet={pet} index={index} handlePetChange={handlePetChange} />
+            ))};
+          </div>
+            
+            <button type="submit">Save</button>
+          
+        </form>
         <h2 className="profile-title">Profile</h2>
         <div className="user-info">
           <h5 className="usernames">
@@ -40,4 +70,4 @@ const ProfileEditor = () => {
   }
 };
 
-export default Profile;
+export default ProfileEditor;
