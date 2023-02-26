d75427a0b7a008dd704653c603dc8388e6eb46f1
Murat Sertay <muzzachan@me.com>
2023-02-26 18:19:35 +0000

    Added geo path to app.js

--- frontend/src/components/app/App.js
+++ frontend/src/components/app/App.js
@@ -5,18 +5,20 @@ import FindPetsPage from '../findPetsPage/FindPetsPage';
 import NavBar from '../navBar/navBar';
 import {Route, Routes, useNavigate,} from "react-router-dom";
 import Profile from '../profile/profile';
+import Geolocation from '../geolocation/Geolocation';
 
 const App = () => {
     return (
         <>
             <NavBar/>
             <Routes>
-                <Route path='/login' element={<LoginForm navigate={useNavigate()} />} />
-                <Route path='/' element={<SignUpForm navigate={useNavigate()} />} />
-                <Route path='/signup' element={<SignUpForm navigate={useNavigate()} />} />
-                <Route path='/findPetsPage' element={<FindPetsPage navigate={useNavigate()} />} />
-                <Route path='/users/:user_id' element={<Profile navigate={useNavigate()} />} />
-            </Routes> 
+                <Route path='/login' element={<LoginForm navigate={useNavigate()}/>}/>
+                <Route path='/' element={<SignUpForm navigate={useNavigate()}/>}/>
+                <Route path='/signup' element={<SignUpForm navigate={useNavigate()}/>}/>
+                <Route path='/findPetsPage' element={<FindPetsPage navigate={useNavigate()}/>}/>
+                <Route path='/users/:user_id' element={<Profile navigate={useNavigate()}/>}/>
+                <Route path='/geo' element={<Geolocation navigate={useNavigate()}/>}/>
+            </Routes>
         </>
     );
 }
