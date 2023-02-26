8bd770896312c795c747ca34cb2e11df8b8f91dc
Oana <oana.demian@gmail.com>
2023-02-26 15:15:53 +0000

    Adds pet info to profile

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -20,26 +20,41 @@ const Profile = () => {
     return (
       <div className="profile">
         <h2 className="profile-title">Profile</h2>
-        <div className="user-info">
+        <div data-cy="user-info" className="user-info">
           <h5 className="usernames">
             {user.firstName} {user.lastName}
           </h5>
           <p className="email">Email address: {user.email}</p>
           <p className="postcode">Postcode: {user.postcode}</p>
-          <p className="petName">Pet's name: {user.pets[0].name}</p>
-          <p className="petWeight">Pet's weight: {user.pets[0].weight}</p>
-          <p className="petAge">Pet's age: {user.pets[0].age}</p>
-          <p className="petDescription">Pet's description: {user.pets[0].description}</p>
-          <p className="petGender">Pet's gender: {user.pets[0].gender}</p>
+        </div>
+        <div className="pets">
+          {user.pets.length > 0 ? (
+            user.pets.map((pet) => (
+              <div data-cy="pet-info" className="pet-info">
+                <p className="petName">Pet's name: {user.pets[0].name}</p>
+                <p className="petWeight">Pet's weight: {user.pets[0].weight}</p>
+                <p className="petAge">Pet's age: {user.pets[0].age}</p>
+                <p className="petDescription">
+                  Pet's description: {user.pets[0].description}
+                </p>
+                <p className="petGender">Pet's gender: {user.pets[0].gender}</p>
+              </div>
+            ))
+          ) : (
+            <div>No pets!</div>
+          )}
         </div>
         <>
-        <a href="/findPetsPage" className="btn btn-primary ">
-          See all pets
-        </a>
+          <a href="/findPetsPage" className="btn btn-primary ">
+            See all pets
+          </a>
         </>
-        <>  <a href="/edit-profile" className="btn btn-primary ">
-          Edit your profile
-        </a></>
+        <>
+          {" "}
+          <a href="/edit-profile" className="btn btn-primary ">
+            Edit your profile
+          </a>
+        </>
       </div>
     );
   }
