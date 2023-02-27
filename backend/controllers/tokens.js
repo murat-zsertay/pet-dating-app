e5dddcf80d4f9708d1620b23e790d70409174c2d
Murat Sertay <muzzachan@me.com>
2023-02-27 09:27:42 +0000

    Updated incorrect user_id referencing

--- backend/controllers/tokens.js
+++ backend/controllers/tokens.js
@@ -1,26 +1,24 @@
-import {User} from "../models/user.js";
-import {TokenGenerator} from "../models/token_generator.js"
+import { User } from '../models/user.js'
+import { TokenGenerator } from '../models/token_generator.js'
 
 export const TokensController = {
 
   Create: (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+    const email = req.body.email
+    const password = req.body.password
 
-    User.findOne({ email: email }).then(async (user) => {
+    User.findOne({ email }).then(async (user) => {
       if (!user) {
         // TODO: User.findOne error handler needs to be tested
-        console.log("auth error: user not found")
-        res.status(401).json({ message: "auth error" });
+        console.log('auth error: user not found')
+        res.status(401).json({ message: 'auth error' })
       } else if (user.password !== password) {
-        console.log("auth error: passwords do not match")
-        res.status(401).json({ message: "auth error" });
+        console.log('auth error: passwords do not match')
+        res.status(401).json({ message: 'auth error' })
       } else {
-        console.log(req.user_id)
-        const token = await TokenGenerator.jsonwebtoken(user.id)
-        res.status(201).json({ token, message: "OK", user_id:user.id });
+        const token = await TokenGenerator.jsonwebtoken(user._id)
+        res.status(201).json({ token, message: 'OK', user_id: user._id })
       }
-    });
+    })
   }
-};
-
+}
