dd8bfeee648b1f0370feecee2adcedfe42493e3c
Murat Sertay <muzzachan@me.com>
2023-02-27 03:47:21 +0000

    Removing Posts and comments files

--- backend/models/comment.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const CommentSchema = new mongoose.Schema({
-  comment: String,
-  post_id: String,
-  user_id: String,
-}, { timestamps: true });
-
-export let Comment = mongoose.model("Comment", CommentSchema);
