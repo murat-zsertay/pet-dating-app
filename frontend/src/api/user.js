06c37b482433125fcd190aeaae89d27bf569c807
Murat Sertay <muzzachan@me.com>
2023-02-27 09:28:47 +0000

    Removed extra getUser function

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,28 +1,11 @@
-export const getUser = async () => {
-  try {
-    const response = await fetch("/users", {
-      headers: {
-        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
-      },
-    });
-    const data = await response.json();
-    window.localStorage.setItem("token", data.token);
-    return data.user;
-  } catch (error) {
-    console.log(error);
-  }
+export const getUserInfoById = async (user_id) => {
+    try {
+        const response = await fetch(`/users/${user_id}`);
+        const data = await response.json();
+        window.localStorage.setItem("token", data.token);
+        return data.user;
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-export const getUserById = async (user_id) => {
-  try {
-    const response = await fetch(`/users/${user_id}`);
-    const data = await response.json();
-    return data.user;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const getPets = () => {
-  getUser().then((user) => user.pets);
-};
