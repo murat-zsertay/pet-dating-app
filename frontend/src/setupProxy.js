9b0368a9c942b26c2c243d675537f542343748f5
Murat Sertay <murat.sertay@icloud.com>
2023-02-27 22:22:19 +0000

    Add proxy url
    Signed-off-by: Murat Sertay <muzzachan@me.com>

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,9 +2,9 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/',
+        '/api',
         createProxyMiddleware({
-            target: 'http://localhost:8080',
+            target: process.env.PROXY_URL,
             changeOrigin: true,
         })
     );
