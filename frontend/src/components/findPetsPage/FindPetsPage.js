fd95a52c7ecacc8f94c01d23572ce6d7e469dead
Oana <oana.demian@gmail.com>
2023-02-27 17:03:26 +0000

    adds request playdate button on findPetsPage

--- frontend/src/components/findPetsPage/FindPetsPage.js
+++ frontend/src/components/findPetsPage/FindPetsPage.js
@@ -16,15 +16,48 @@ const FindPetsPage = ({navigate}) => {
         fetchPets();
     }, []);
 
+    const handlePlaydateRequest = async () => {
+      try {
+        const response = await fetch(`/pets/request`, {
+          method: "post",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ 
+            petId: pets[0]._id,
+            petName: pets[0].name, 
+            petWeight: pets[0].weight, 
+            petAge: pets[0].age, 
+            petGender: pets[0].gender, 
+          }),
+        });
+    
+        if (response.ok) {
+          const data = await response.json();
+          console.log('Playdate request sent')
+        } else {
+          console.log(`Failed to send playdate request`);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    
+
     if (token) {
         return (
             <div className="find-pets-title-div">
                 <h2 className="post-title">Find Pets</h2>
                 <div id="find-pets" role="find-pets">
-
-                    {pets.map((pet) => (
-                        <Pet pet={pet} key={pet._id}/>
-                    ))}
+                  {pets.map((pet) => (
+<>
+                    <Pet pet={pet} key={pet._id.toString()}/>
+                    <div className="playdateButton">
+                    <button id="playdate" onClick={()=>handlePlaydateRequest()}>Request playdate</button>
+                    </div>
+</>
+                  ))}
                 </div>
             </div>
         );
