389c1fece4a8fba98b470e5706617be6ae1505eb
Murat Sertay <muzzachan@me.com>
2023-02-27 10:07:33 +0000

    removing env variables from ecosystem config

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,23 +1,8 @@
 module.exports = {
-  apps : [{
-    name:"Express backend",
-    script: 'app.js',
-    watch: '.',
-    "env_test": {
-      NODE_ENV: "test",
-      JWT_SECRET:"SUPER_SECRET",
-      PORT:8080,
-      MONGODB_URL:"mongodb://0.0.0.0:27017/pet_test"
-    },
-    "env_production": {
-      NODE_ENV: "production",
-    },
-    "env_development": {
-      NODE_ENV: "development",
-      JWT_SECRET:"SUPER_SECRET",
-      MONGODB_URL:"mongodb://0.0.0.0:27017/pet_development",
-      PORT:8080
-    }
-  }],
+    apps: [{
+        name: "Express backend",
+        script: 'app.js',
+        watch: '.',
+    }],
 
 };
