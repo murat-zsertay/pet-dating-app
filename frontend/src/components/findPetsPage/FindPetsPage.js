95bb481593d47fa2b76effd2e0881a9b220fd7a4
Murat Sertay <muzzachan@me.com>
2023-02-27 08:14:30 +0000

    Updated FindPetsPage.js so to reflect how Backend works

--- frontend/src/components/findPetsPage/FindPetsPage.js
+++ frontend/src/components/findPetsPage/FindPetsPage.js
@@ -1,36 +1,40 @@
 import React, { useEffect, useState } from "react";
 import "./FindPetsPage.css";
-import { Pet } from "../pet/Pet.js";
-import { getUserById } from "../../api/user.js";
+import Pet from "../pet/Pet.js";
+import {getAllPets} from "../../api/pets";
 
-const FindPetsPage = ({ navigate }) => {
-  const [pets, setPets] = useState([]);
-  const [token] = useState(window.localStorage.getItem("token"));
+const FindPetsPage = ({navigate}) => {
+    const [pets, setPets] = useState([]);
+    const [token] = useState(window.localStorage.getItem("token"));
 
-  const fetchPets = async () => {
-    const user_id = window.localStorage.getItem("user_id");
-    const petsData = await getUserById(user_id);
-    setPets(petsData.pets);
-  };
+    const fetchPets = async () => {
+        const {pets} = await getAllPets(token);
+        setPets(pets);
+    };
 
-  useEffect(() => {
-    fetchPets();
-  }, []);
+    useEffect(() => {
+        fetchPets().then(r => {
+            console.log("Pets Fetched")
+        });
+    }, []);
 
-  if (token) {
-    return (
-      <div className="find-pets-title-div">
-        <h2 className="post-title">Find Pets</h2>
-        <div id="find-pets" role="find-pets">
-          {pets.map((pet) => (
-            <Pet pet={pet} key={pet._id} />
-          ))}
-        </div>
-      </div>
-    );
-  } else {
-    navigate("/login");
-  }
+    if (token) {
+        return (
+            <div className="find-pets-title-div">
+                <h2 className="post-title">Find Pets</h2>
+                <div id="find-pets" role="find-pets">
+                    // TODO: Note that on the array of pet Objects I am passing through the
+                    owner userId, check the Postman request GET /pets
+
+                    {pets.map((pet) => (
+                        <Pet pet={pet} key={pet._id}/>
+                    ))}
+                </div>
+            </div>
+        );
+    } else {
+        navigate("/login");
+    }
 };
 
 export default FindPetsPage;
