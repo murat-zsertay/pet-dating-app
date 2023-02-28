3f58cff51be775e136c6317dff1058edefa7597c
charlie lambert <char.lam@ntlworld.com>
2023-02-28 13:53:17 +0000

    Button for uploading works and shows on cloudinary

--- backend/routes/pets.js
+++ backend/routes/pets.js
@@ -3,7 +3,11 @@ import express from 'express'
 import { PetsController } from '../controllers/pets.js'
 import { tokenChecker } from '../utils/expresssMiddleware.js'
 
+import upload from '../config/multer.js'
+
 export const petsRouter = express.Router()
 
 petsRouter.get('/', tokenChecker, PetsController.Index)
 petsRouter.post('/request', tokenChecker, PetsController.RequestMeetup)
+petsRouter.post('/profile-image-upload', tokenChecker, upload.single('image'), PetsController.UploadImage)
+petsRouter.put('./profile-image-edit', tokenChecker, PetsController.UpdateImage)
