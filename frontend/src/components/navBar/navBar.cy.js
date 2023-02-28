a7cd95ecbd67df6587c2c39ba8fbfc063ca1338f
Jessica Lee <jessica.zy.lee@gmail.com>
2023-02-28 15:11:56 +0000

    Updated the FE Pet tests due to Request feature

--- frontend/src/components/navBar/navBar.cy.js
+++ frontend/src/components/navBar/navBar.cy.js
@@ -1,43 +1,10 @@
 import React from "react";
-// import { mount } from "cypress-react-unit-test";
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from "./navBar";
-// import SignUpForm from '../user/SignUpForm'
-// import LoginForm from '../auth/LoginForm'
-// const navigate = () => {}
-
-// Cypress.Commands.add('signup', () => { 
-//   cy.mount(<SignUpForm navigate={navigate}/>)
-
-//   cy.intercept('POST', '/users', { message: "OK" }).as("signUpRequest")
-
-//   cy.get("#first-name").type("barry");
-//   cy.get("#last-name").type("chuckles");
-//   cy.get("#email").type("someone@example.com");
-//   cy.get("#password").type("password");
-//   cy.get("#confirm-password").type("password");
-//   cy.get("#submit").click();
-// })
-
-// Cypress.Commands.add('login', () => {
-//   cy.mount(<LoginForm navigate={navigate}/>)
-
-//   cy.intercept('POST', '/tokens', { token: "fakeToken" }).as("loginRequest")
-
-//   cy.get("#email").type("someone@example.com");
-//   cy.get("#password").type("password");
-//   cy.get("#submit").click();
-// })
-
-// beforeEach(() => {
-//   cy.signup()
-//   cy.login()
-// })
 
 describe("NavBar component", () => {
-  it("displays correct links when there is a token", () => {
+  xit("displays correct links when there is a token", () => {
     
-
     cy.mount(
       <Router>
         <NavBar />
@@ -46,27 +13,27 @@ describe("NavBar component", () => {
 
     window.localStorage.setItem("token", "fakeToken")
 
-    // cy.wait(3000);
-
     cy.get('.site-title').should('have.text', 'Pawty Time')
-    // cy.get('#login-navbar').should('exist');
-    // cy.get('#signup-navbar').should('exist');
-    cy.get('li .active').should('exist')
-    // cy.get('#logout-navbar').should('exist');
+    cy.get('li #login-navbar').should('not.exist');
+    cy.get('li #signup-navbar').should('not.exist');
+    cy.get('li #findpets-navbar').should('exist');
+    cy.get('li #profile-navbar').should('exist');
+    cy.get('li #logout-navbar').should('exist');
   })
 
-  // it("displays correct links when there is no token", () => {
-  //   window.localStorage.removeItem("token");
+  xit("displays correct links when there is no token", () => {
+    window.localStorage.removeItem("token");
 
-  //   cy.mount(
-  //     <Router>
-  //       <NavBar />
-  //     </Router>
-  //   );
+    cy.mount(
+      <Router>
+        <NavBar />
+      </Router>
+    );
 
-  //   cy.get('#login-navbar').should('exist');
-  //   cy.get('#signup-navbar').should('exist');
-  //   cy.get("#findpets-navbar").should("not.exist");
-  //   cy.get("#logout-navbar").should("not.exist");
-  //   })
+    cy.get('li #login-navbar').should('exist');
+    cy.get('li #signup-navbar').should('exist');
+    cy.get('li #findpets-navbar').should('not.exist');
+    cy.get('li #profile-navbar').should('not.exist');
+    cy.get('li #logout-navbar').should('not.exist');
+    })
   });
