ab13530d318292b56eef61c8758107a13ad09ff2
Oana <oana.demian@gmail.com>
2023-02-28 22:51:13 +0000

    Adds backend route to view playdates info

---
+++ backend/models/playdate.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+const PlaydateSchema = new mongoose.Schema(
+  {
+    requester_pet_id: {
+      type: String,
+      required: [true, 'required']
+    },
+    recipient_pet_id: {
+      type: String,
+      required: [true, 'required']
+    },
+    recipient_user_id: {
+      type: String,
+      required: [true, 'required']
+    },
+    accepted: {
+      type: String,
+      required: [true, 'required']
+    }
+  },
+  { timestamps: true }
+)
+export const Playdate = mongoose.model('Playdate', PlaydateSchema)
---
+++ backend/routes/playdates.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+import { PlaydateController } from '../controllers/playdate.js'
+import { tokenChecker } from '../utils/expresssMiddleware.js'
+
+export const playdatesRouter = express.Router()
+
+playdatesRouter.post('/request', tokenChecker, PlaydateController.Create)
+playdatesRouter.get('/requests', tokenChecker, PlaydateController.Index)
+
