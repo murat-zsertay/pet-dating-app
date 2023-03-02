dbe65ce58cbb362890a6e39d1bd1ecfabddbe9fe
Murat Sertay <muzzachan@me.com>
2023-03-02 09:05:36 +0000

    Adding proxy setup
    Signed-off-by: Murat Sertay <muzzachan@me.com>

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,4 +1,4 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+const { createProxyMiddleware } = import('http-proxy-middleware')
 
 module.exports = function (app) {
   app.use(
